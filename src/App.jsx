70aad8d225dd5875ad349412aa3fbb55244f5604
Madina050801 <madinaabduraufova@gmail.com>
2021-12-01 14:25:28 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Hi React</h1>
+      </div>
+    );  
+  }
+}
+
+class Student extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Hi Student</h1>
+      </div>
+    );  
+  }
+}
+
+export {Student};
+
+export default App;
