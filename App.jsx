b06711aabd251ce98813f92328b14da94d94548c
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2021-12-03 13:22:53 +0500

    reload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Student extends React.Component {
+  render(){
+    console.log(this);
+    return(
+      <div>
+        <h1>Lorem ipsum dolor sit amet hijbj khjbjk consectetur adipisicing elit. Fuga, in? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, modi!</h1>
+      </div>
+    );  
+  }
+}
+
+
+export default Student;
